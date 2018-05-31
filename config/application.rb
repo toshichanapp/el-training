63489bbe23bd4d77e85d591d2823acc799044fe6
toshichanapp <toshichan@synapz.jp>
2018-05-31 15:56:30 +0900

    template engineをslimに変更

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,9 @@ module ElTraining
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.system_tests nil
+      g.template_engine :slim
+    end
   end
 end
