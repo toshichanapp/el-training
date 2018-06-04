1394698ac0b55d37b7c161c655c33b2540e5e528
Toshiki Fujiyoshi <toshichan@synapz.jp>
2018-06-04 13:33:57 +0900

    generateコマンド実行時、自動生成されるテストを制限する

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,11 @@ module ElTraining
       g.stylesheets false
       g.javascripts false
       g.helper false
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
     end
   end
 end
