748fb3b7dc62a50af6a5bc6f0ac6f2a1d48c6041
Toshiki Fujiyoshi <toshichan@synapz.jp>
2018-06-05 16:56:36 +0900

    アソシエーション記述

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,8 @@
 class Task < ApplicationRecord
 
+  has_many :user_tasks
+  has_many :users, through: :user_tasks
+
   enum status: {untouched: 0, started: 1, completion: 2}
   enum priority: {low: 0, normal: 1, high: 2}
   validates :name, presence: true
